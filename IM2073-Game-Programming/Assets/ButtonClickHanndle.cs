a1c416edb945f5e4fc4af8d56f5b155ab9bc6433
Yuyue <YYU035@e.ntu.edu.sg>
2025-02-09 23:08:34 +0800

    changed

---
+++ IM2073-Game-Programming/Assets/ButtonClickHanndle.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClickHandler : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        // 在这里编写按钮点击后要执行的逻辑，比如加载新场景、显示新界面等
+        Debug.Log("Button Clicked!");
+    }
+}
